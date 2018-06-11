c8863ad14386b17e61507340eb57332a6e845862
paulo.vsfigueiredo <paulo.vsfigueiredo@TAT0541210W10-1.senacsp.edu.br>
2018-06-11 17:22:51 -0300

    aula do dia 11/6/2018

---
+++ Fundamentos do Java/src/br/com/java/TabuadaWhile.java
@@ -0,0 +1,21 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class TabuadaWhile {
+
+	public static void main(String[] args) {
+		int valor;
+		int i = 1;//variável de apoio ao laço while
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("______TABUADA______");
+		System.out.println("");
+		System.out.println("Digite o valor da tabuada: ");
+		valor = teclado.nextInt();
+		while (i < 11) {	
+			System.out.println(valor + " x " + i + " = " + (valor * i));
+			i++;
+		}
+	}
+
+}

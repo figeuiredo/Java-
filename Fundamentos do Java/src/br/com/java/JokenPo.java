0419281bb62e29d489d81b7a5c8726e78f2758d6
Figue <Figue@192.168.0.79>
2018-06-13 15:54:46 -0300

    aula do dia 13-6-2018

---
+++ Fundamentos do Java/src/br/com/java/JokenPo.java
@@ -0,0 +1,87 @@
+package br.com.java;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JokenPo {
+
+	public static void main(String[] args) {
+		int jogador;
+		char opcao;
+		Scanner teclado = new Scanner(System.in);
+		do {
+		System.out.println("JoKenPo");
+		System.out.println("");
+		System.out.println("1. Pedra");
+		System.out.println("2. Papel");
+		System.out.println("3. Tesoura");
+		System.out.println("----------------------------");
+		System.out.print("Digite a opção desejada: ");
+		jogador = teclado.nextInt();
+		System.out.println("----------------------------");
+		switch (jogador) {
+		case 1:
+			System.out.println("Jogador escolheu Pedra");
+			break;
+		case 2:
+			System.out.println("Jogador escolheu Papel");
+			break;
+		case 3:
+			System.out.println("Jogador escolheu Tesoura");
+			break;
+		default:
+			System.out.println("Opção inválida");
+			break;
+			
+		}
+		Random maquina = new Random();
+		int computador = maquina.nextInt(2) + 1;
+		switch (computador) {
+		case 1:
+			Pedra();
+			System.out.println(" ");
+			System.out.println("Computador escolheu Pedra");
+			break;
+		case 2:
+			Papel();
+			System.out.println(" ");
+			System.out.println("Computador escolheu Papel");
+			break;
+		case 3:
+			Tesoura();
+			System.out.println(" ");
+			System.out.println("Computador escolheu Tesoura");
+			break;
+		default:
+			System.out.println(" ");
+			System.out.println("Opção inválida");
+			break;
+		
+		}
+		
+		if (jogador == 1 && computador == 3 || jogador == 2 && computador == 1 || jogador == 2 && computador == 3 ) {
+			System.out.println("____________________________");
+			System.out.println("JOGADOR VENCEU");
+		}else if (computador == 1 && jogador == 3 || computador == 2 && jogador == 1 || computador == 2 && jogador == 3 ) {
+			System.out.println("____________________________");
+			System.out.println("COMPUTADOR VENCEU");
+		} else {
+			System.out.println("_______________________");
+			System.out.println("EMPATE");
+		}
+		System.out.print("Deseja jogar novamente(s/n)? ");
+		opcao = teclado.next().charAt(0);
+	}while(opcao == 's');
+	}
+
+	static void Tesoura() {
+	}
+
+	static void Papel() {
+	}
+
+	static void Pedra() {
+
+	}
+
+}

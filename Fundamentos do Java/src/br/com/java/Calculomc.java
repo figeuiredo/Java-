5b6260a9f18165485c30bd2ff799fbe9d6d8f1ce
Figue <Figue@192.168.0.79>
2018-06-16 18:03:06 -0300

    calculo imc

--- Fundamentos do Java/src/br/com/java/Calculomc.java
+++ Fundamentos do Java/src/br/com/java/Calculomc.java
@@ -19,6 +19,18 @@ public class Calculomc {
 		imc = peso / (altura * altura);
 		//saída
         System.out.println(" o valor da sua massa corporal é: " + imc);
+        if (imc < 18.49) {
+			System.out.println("Abaixo do Peso");
+		} else if (imc >= 18.5 && imc <= 24.99) {
+            System.out.println("Peso Normal");
+		} else if (imc >= 25 && imc <= 29.99) {
+            System.out.println("acima do Peso");
+	    } else if (imc >= 30  && imc <= 34.99) {
+	        System.out.println("Obesidade 1");
+	    } else if (imc >= 35  && imc <= 39.99) {
+            System.out.println("Obesidade 2");
+        } else {
+            System.out.println("Obesidade 3");
+       }
 	}
-
 }
